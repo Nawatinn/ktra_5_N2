39d873314fd44fa9b992433f04e91f44990d2876
nawatinn <030239230229@st.buh.edu.vn>
2026-04-07 08:35:58 +0700

    Cau 4

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
+
+Route::get('/them-phim', [App\Http\Controllers\MovieController3::class, 'create']);
+
+Route::post('/them-phim', [App\Http\Controllers\MovieController3::class, 'store']);
